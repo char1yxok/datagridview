75f81ccac59d069f8b526540bb486206f87accd0
Yuta Nakano <chm.af1yel.x@gmail.com>
2019-01-31 16:48:08 +0900

    first commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataTableBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.valueDataset = new WindowsFormsApp3.valueDataset();
+            this.valueDataTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.valueDatasetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.valueDataTableBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataTableBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDatasetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataTableBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nodeDataGridViewTextBoxColumn,
+            this.typeDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn,
+            this.unitDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.valueDataTableBindingSource2;
+            this.dataGridView1.Location = new System.Drawing.Point(48, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(669, 271);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nodeDataGridViewTextBoxColumn
+            // 
+            this.nodeDataGridViewTextBoxColumn.DataPropertyName = "node";
+            this.nodeDataGridViewTextBoxColumn.HeaderText = "node";
+            this.nodeDataGridViewTextBoxColumn.Name = "nodeDataGridViewTextBoxColumn";
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "value";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // unitDataGridViewTextBoxColumn
+            // 
+            this.unitDataGridViewTextBoxColumn.DataPropertyName = "unit";
+            this.unitDataGridViewTextBoxColumn.HeaderText = "unit";
+            this.unitDataGridViewTextBoxColumn.Name = "unitDataGridViewTextBoxColumn";
+            // 
+            // valueDataTableBindingSource2
+            // 
+            this.valueDataTableBindingSource2.DataMember = "valueDataTable";
+            this.valueDataTableBindingSource2.DataSource = this.valueDataset;
+            // 
+            // valueDataset
+            // 
+            this.valueDataset.DataSetName = "valueDataset";
+            this.valueDataset.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // valueDataTableBindingSource
+            // 
+            this.valueDataTableBindingSource.DataMember = "valueDataTable";
+            this.valueDataTableBindingSource.DataSource = this.valueDataset;
+            // 
+            // valueDatasetBindingSource
+            // 
+            this.valueDatasetBindingSource.DataSource = this.valueDataset;
+            this.valueDatasetBindingSource.Position = 0;
+            // 
+            // valueDataTableBindingSource1
+            // 
+            this.valueDataTableBindingSource1.DataMember = "valueDataTable";
+            this.valueDataTableBindingSource1.DataSource = this.valueDataset;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(511, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 41);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(48, 331);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(84, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(138, 331);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(84, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox3.Location = new System.Drawing.Point(228, 331);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(84, 25);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox4.Location = new System.Drawing.Point(318, 331);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(84, 25);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox5.Location = new System.Drawing.Point(408, 331);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(84, 25);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 313);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "node";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 313);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 313);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "value";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "unit";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(627, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 41);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataTableBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDatasetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueDataTableBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource valueDataTableBindingSource;
+        private valueDataset valueDataset;
+        private System.Windows.Forms.BindingSource valueDatasetBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource valueDataTableBindingSource1;
+        private System.Windows.Forms.BindingSource valueDataTableBindingSource2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            valueDataset.valueDataTable.AddvalueDataTableRow("Root", "CEO", "CEO", "5000", "USD");
+            //valueDataset.valueDataTable.AddvalueDataTableRow("Root", "a", "a", "5000", "USD");
+            //valueDataset.valueDataTable.AddvalueDataTableRow("a","b", "b", "5000", "USD");
+            //valueDataset.valueDataTable.AddvalueDataTableRow("a", "C", "c", "5000", "USD");
+
+            //DatasetのRoot
+            /*var RootDataset = new valueDataset();
+
+            var Root = RootDataset.valueDataTable.NewRow();
+
+            Root[0] = "Root";
+            Root[1] = "CEO";
+            Root[2] = "CEO";
+            Root[3] = "5000";
+            Root[4] = "USD";
+
+            RootDataset.valueDataTable.Rows.Add(Root);
+
+            RootDataset.valueDataTable.AddvalueDataTableRow(Root);
+            var NodeColumnName = RootDataset.valueDataTable.nodeColumn.ColumnName;
+            var TypeColumnName = RootDataset.valueDataTable.typeColumn.ColumnName;
+            var NameColumnName = RootDataset.valueDataTable.nameColumn.ColumnName;
+            var ValueColumnName = RootDataset.valueDataTable.valueColumn.ColumnName;
+            var UnitColumnName = RootDataset.valueDataTable.unitColumn.ColumnName;
+
+            var Rootrow = RootDataset.valueDataTable.Rows(0);*/
+
+            
+
+        }
+
+
+
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //string json = JsonConvert.SerializeObject(valueDataset);
+            //Console.WriteLine(json);
+
+            var nodeb = textBox1.Text;
+            var typeb = textBox2.Text;
+            var nameb = textBox3.Text;
+            var valueb = textBox4.Text;
+            var unitb = textBox5.Text;
+
+            DataRow dr = valueDataset.valueDataTable.NewRow();
+            dr[0] = nodeb;
+            dr[1] = typeb;
+            dr[2] = nameb;
+            dr[3] = valueb;
+            dr[4] = unitb;
+
+            valueDataset.valueDataTable.Rows.Add(dr);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataGridViewRow removerow = dataGridView1.CurrentRow;
+            dataGridView1.Rows.Remove(removerow);
+        }
+    }
+}
